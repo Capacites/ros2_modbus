00b9874cabd5993cd2bc54cd9a5c4ed673090a4e
AlexisFetet <fetet.alexis@gmail.com>
2022-04-08 11:16:56 +0200

    digital input on timer read

--- ros_modbus/src/modbus_node.py
+++ ros_modbus/src/modbus_node.py
@@ -19,13 +19,29 @@ class modbus_node(Node):
         self.m_name = self.declare_parameter('name', 'test_device').value
         self.m_YAML_config_file = self.declare_parameter('YAML_config_file', '/home/ecn/ros2/src/modbus_ros2/ros_modbus/map.yaml').value
         self.m_debug = self.declare_parameter('debug', True).value
+
+        #member varaiables declarations in case of faulty configuration
+        self.m_address = None
+        self.m_port = None
+        self.m_publish_on_timer = None
+        self.m_msg_on_timer = Modbus()
+        self.m_msg_on_timer.header.frame_id = 'Modbus_report'
+        self.m_msg_on_timer.coils = []
+        self.m_msg_on_timer.values = []
+        self.m_publish_on_event = None
+        self.m_msg_on_event = Modbus()
+        self.m_msg_on_event.header.frame_id = 'Modbus_report'
+        self.m_msg_on_event.coils = []
+        self.m_msg_on_event.values = []
+        self.m_IO = None
+        self.m_clientMaster = None
        
         self.m_configOK = False
 
         try:
             self.configure()
         except:
-            self.get_logger().error("config file not valid")
+            self.get_logger().error("Configuration file not valid, please provide a valid configuration file")
 
         #ROS related components
         self.m_subscriber = self.create_subscription(Modbus, "/ros_modbus/command",self.subscriber_callback, self.m_sub_queue_size)
@@ -37,9 +53,9 @@ class modbus_node(Node):
             conf_dic = yaml.safe_load(file)
             self.m_address = conf_dic[self.m_name]['address']
             self.m_port = conf_dic[self.m_name]['port']
-            self.create_timer(1/conf_dic[self.m_name]['publish_frequency'], self.timer_callback)
-            self.m_publish_on_timer = conf_dic[self.m_name]['publish_on_timer']
-            self.m_publish_on_event = conf_dic[self.m_name]['publish_on_event']
+            self.create_timer(1/conf_dic[self.m_name]['publish_frequency'], self.publish_timer_callback)
+            self.m_publish_on_timer = dict(zip(conf_dic[self.m_name]['publish_on_timer'],[0]*len(conf_dic[self.m_name]['publish_on_timer'])))
+            self.m_publish_on_event = dict(zip(conf_dic[self.m_name]['publish_on_event'],[0]*len(conf_dic[self.m_name]['publish_on_event'])))
             self.m_IO = {}
 
             for type, data_type in conf_dic[self.m_name]['input'].items():
@@ -54,43 +70,56 @@ class modbus_node(Node):
             self.get_logger().info(f'Configured automat {self.m_name} with IO {[k for k in self.m_IO.keys()]}')
             self.m_configOK = True
 
-    def timer_callback(self):
+
+    def now(self):
+            return self.get_clock().now().to_msg()
+
+
+    def publish_timer_callback(self):
         
-        if self.m_configOK:
+        if self.m_configOK :
             self.m_clientMaster.open()
-            if self.m_debug:
-                self.get_logger().info(f"running with {self.m_address}:{self.m_port} currently open: {self.m_clientMaster.is_open()}")
-            coils_l = self.m_clientMaster.read_coils(0, 16)
-            if coils_l:
-                print(coils_l)
-                for k in range(len(coils_l)):
-                    print(f"elem {k} is {coils_l[k]}")
+            if self.m_clientMaster.is_open():
+                for key in self.m_publish_on_timer.keys():
+                    if self.m_IO[key][0] == 'input':
+                        if self.m_IO[key][1] == 'digital':
+                            self.m_publish_on_timer.update({key: int(self.m_clientMaster.read_coils(self.m_IO[key][2])[0])})
+                            print(self.m_publish_on_timer[key])
+            self.m_msg_on_timer.coils = list(self.m_publish_on_timer.keys())
+            print(self.m_publish_on_timer.values())
+            self.m_msg_on_timer.values = list(self.m_publish_on_timer.values())
+            self.m_msg_on_timer.header.stamp = self.now()
             self.m_clientMaster.close()
+            self.m_publisher.publish(self.m_msg_on_timer)
+
         else:
-            self.get_logger().warn(f'Current config not valid, waiting for new parameters')
+            self.get_logger().warn(f'Timer callback but configuration is not valid, skipping')
 
 
-    def subscriber_callback(self, msg):       
-        command = dict(zip(msg.coils, msg.values))
-        for coil, value in command.items():
-            if coil in self.m_IO.keys():
-                if self.m_IO[coil][0] == 'output':
-                    if self.m_IO[coil][1] == 'digital':
-                        try:
-                            self.m_clientMaster.write_single_coil(self.m_IO[coil][2], bool(value))
-                        except:
-                            self.get_logger().error(f'Tried to set coil {coil} at value {bool(value)} (value given was {value}) and failed , skipping')
-                    elif self.m_IO[coil][1] == 'analog':
-                        try:
-                            self.m_clientMaster.write_single_coil(self.m_IO[coil][2], value)
-                        except:
-                            self.get_logger().error(f'Tried to set coil {coil} at value {value} and failed , skipping')
+    def subscriber_callback(self, msg):   
+        if self.m_configOK:    
+            command = dict(zip(msg.coils, msg.values))
+            for coil, value in command.items():
+                if coil in self.m_IO.keys():
+                    if self.m_IO[coil][0] == 'output':
+                        if self.m_IO[coil][1] == 'digital':
+                            try:
+                                self.m_clientMaster.write_single_coil(self.m_IO[coil][2], bool(value))
+                            except:
+                                self.get_logger().error(f'Tried to set coil {coil} at value {bool(value)} (value given was {value}) and failed, skipping')
+                        elif self.m_IO[coil][1] == 'analog':
+                            try:
+                                self.m_clientMaster.write_single_coil(self.m_IO[coil][2], value)
+                            except:
+                                self.get_logger().error(f'Tried to set coil {coil} at value {value} and failed, skipping')
+                        else:
+                            self.get_logger().warn(f'Unsupported output type for coil {coil}, skipping')
                     else:
-                        self.get_logger().warn(f'Unsupported output type for coil {coil}, skipping')
+                        self.get_logger().warn(f'Coil {coil} is not set as output, skipping')
                 else:
-                    self.get_logger().warn(f'Coil {coil} is not set as output, skipping')
-            else:
-                self.get_logger().warn(f'Coil {coil} not declared, skipping')
+                    self.get_logger().warn(f'Coil {coil} not declared, skipping')
+        else:
+            self.get_logger().warn(f'Messaged received but configuration is not valid, skipping')
 
 
     
