1daec2226dd91b2dce7d83d3199dbffdd24aad55
AlexisFetet <fetet.alexis@gmail.com>
2022-04-19 17:47:35 +0200

    removing buffers

--- ros_modbus/src/modbus_interface.cpp
+++ ros_modbus/src/modbus_interface.cpp
@@ -92,70 +92,62 @@ void ModbusInterface::updateMemory()
     m_memory_guard.lock();
     m_temp_nb_to_get =  m_memory.nb_input_coils;
     m_memory_guard.unlock();
-    uint8_t bufferid[m_temp_nb_to_get];
-    uint8_t *m_temp8id = bufferid;
+
     m_ctx_guard.lock();
-    m_success = modbus_read_input_bits(m_ctx, 0, m_temp_nb_to_get , m_temp8id) != -1;
+    m_memory_guard.lock();
+    m_success = modbus_read_input_bits(m_ctx, 0, m_temp_nb_to_get , m_memory.input_coils.data()) != -1;
+    m_memory_guard.unlock();
     m_ctx_guard.unlock();
     if(!m_success)
     {
         throw MODBUS_EXCEPTION_SLAVE_OR_SERVER_FAILURE;
     }
-    m_memory_guard.lock();
-    std::copy(m_temp8id, m_temp8id+m_temp_nb_to_get, m_memory.input_coils.begin());
-    m_memory_guard.unlock();
 
     //update output coils
     m_memory_guard.lock();
     m_temp_nb_to_get =  m_memory.nb_output_coils;
     m_memory_guard.unlock();
-    uint8_t bufferod[m_temp_nb_to_get];
-    uint8_t *m_temp8od = bufferod;
+
     m_ctx_guard.lock();
-    m_success = modbus_read_bits(m_ctx, 0,m_temp_nb_to_get , m_temp8od) != -1;
+    m_memory_guard.lock();
+    m_success = modbus_read_bits(m_ctx, 0,m_temp_nb_to_get , m_memory.output_coils.data()) != -1;
+    m_memory_guard.unlock();
     m_ctx_guard.unlock();
     if(!m_success)
     {
         throw MODBUS_EXCEPTION_SLAVE_OR_SERVER_FAILURE;
     }
-    m_memory_guard.lock();
-    std::copy(m_temp8od, m_temp8od+m_temp_nb_to_get, m_memory.output_coils.begin());
-    m_memory_guard.unlock();
 
     //update input registers
     m_memory_guard.lock();
     m_temp_nb_to_get =  m_memory.nb_input_registers;
     m_memory_guard.unlock();
-    uint16_t bufferia[2*m_temp_nb_to_get];
-    uint16_t *m_temp16ia = bufferia;
+
     m_ctx_guard.lock();
-    m_success = modbus_read_input_registers(m_ctx, 0,m_temp_nb_to_get , m_temp16ia) != -1;
+    m_memory_guard.lock();
+    m_success = modbus_read_input_registers(m_ctx, 0,m_temp_nb_to_get , m_memory.input_registers.data()) != -1;
+    m_memory_guard.unlock();
     m_ctx_guard.unlock();
     if(!m_success)
     {
         throw MODBUS_EXCEPTION_SLAVE_OR_SERVER_FAILURE;
     }
-    m_memory_guard.lock();
-    std::copy(m_temp16ia, m_temp16ia+m_temp_nb_to_get, m_memory.input_registers.begin());
-    m_memory_guard.unlock();
+
 
     //update output registers
     m_memory_guard.lock();
     m_temp_nb_to_get =  m_memory.nb_output_registers;
     m_memory_guard.unlock();
-    uint16_t bufferoa[2*m_temp_nb_to_get];
-    uint16_t *m_temp16oa = bufferoa;
+
     m_ctx_guard.lock();
-    m_success = modbus_read_registers(m_ctx, 0,m_temp_nb_to_get , m_temp16oa) != -1;
+    m_memory_guard.lock();
+    m_success = modbus_read_registers(m_ctx, 0,m_temp_nb_to_get , m_memory.output_registers.data()) != -1;
+    m_memory_guard.unlock();
     m_ctx_guard.unlock();
     if(!m_success)
     {
         throw MODBUS_EXCEPTION_SLAVE_OR_SERVER_FAILURE;
     }
-    m_memory_guard.lock();
-    std::copy(m_temp16oa, m_temp16oa+m_temp_nb_to_get, m_memory.output_registers.begin());
-    m_memory_guard.unlock();
-
 }
 
 uint8_t ModbusInterface::getSingleInputCoil(int address)
