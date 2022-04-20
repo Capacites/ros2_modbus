100c58963ff861748f5d820e5dfbcdae39c05edb
AlexisFetet <fetet.alexis@gmail.com>
2022-04-20 13:49:15 +0200

    ajout license

--- ros_modbus/include/modbus_node/modbus_node.h
+++ ros_modbus/include/modbus_node/modbus_node.h
@@ -1,4 +1,29 @@
 // -*- lsst-c++ -*-
+/*
+ * This file is part of modbus_ros2.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 CAPACITES
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 /**
  * @file modbus_node.h
  *
@@ -120,7 +145,6 @@ private:
     int m_pub_queue_size = declare_parameter<int>("pub_queue_size", 5);                                                                           /*!< Queue size for publishers                   */
     std::string m_name = declare_parameter<std::string>("name", "test_device");                                                                   /*!< Device name                                 */
     std::string m_YAML_config_file = declare_parameter<std::string>("YAML_config_file", "/home/ecn/ros2/src/modbus_ros2/ros_modbus/map.yaml");    /*!< YAML device desription file full path       */
-    bool m_debug = declare_parameter<bool>("debug", false);                                                                                       /*!< Debug mode                                  */
 
 //Node member variables
     std::map<std::string, uint16_t> m_publish_on_timer;       /*!< Map of IO and their values to publish on timer callback */
