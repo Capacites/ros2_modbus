15249f23a6b77d4aca9a709de239b4ac2abf0a6a
Alexis <alexis.fetet@capacites.fr>
2022-06-15 11:13:21 +0200

    sync bug fix

--- ros_modbus/src/modbus_node.cpp
+++ ros_modbus/src/modbus_node.cpp
@@ -457,6 +457,8 @@ void ModbusNode::subscriber_callback(ros_modbus_msgs::msg::Modbus::SharedPtr p_m
     }
     while(count < 4 && !m_testa);
 
+    m_modbus_device.updateMemory();
+
     if(!m_testa || !m_testd) // if we didn't managed to send the command
     {
 
