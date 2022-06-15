0163b282fca9648a7774d1510a384fd5f64f6248
Alexis FETET <alexis.fetet@univ-nantes.fr>
2022-06-15 09:25:14 +0000

    Dev

--- ros_modbus/src/modbus_node.cpp
+++ ros_modbus/src/modbus_node.cpp
@@ -457,6 +457,13 @@ void ModbusNode::subscriber_callback(ros_modbus_msgs::msg::Modbus::SharedPtr p_m
     }
     while(count < 4 && !m_testa);
 
+    try
+    {
+        m_modbus_device.updateMemory();
+    }
+    catch(...) // If error occurs
+    {}
+
     if(!m_testa || !m_testd) // if we didn't managed to send the command
     {
 
